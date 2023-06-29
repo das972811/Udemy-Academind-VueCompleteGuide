054e81a3104fd907602aee8d8b6ed61054266a3d
das972811 <salasdiego54@gmail.com>
2023-06-29 11:51:26 -0500

    Basic intro to Vue CLI

---
+++ 7. Moving to a Better Development Setup & Workflow with the Vue CLI/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Detail</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+
+    </li>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                detailsAreVisible: false,
+                friend: {
+                        id: 'diego',
+                        name: 'Diego Salas',
+                        email: 'diego@localhost.com',
+                        phone: '1234 567 890'
+                    },
+            };
+        },
+        methods: {
+            toggleDetails() {
+                this.detailsAreVisible = !this.detailsAreVisible;
+            }
+        }
+    };
+</script>
