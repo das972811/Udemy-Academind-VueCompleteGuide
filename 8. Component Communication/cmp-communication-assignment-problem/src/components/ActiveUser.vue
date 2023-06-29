2c3ee94b1ad4297491a141883cb9702bc84fc1bc
das972811 <salasdiego54@gmail.com>
2023-06-29 15:17:27 -0500

    Programming Assignment

---
+++ 8. Component Communication/cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                userName: '',
+                age: ''
+            };
+        },
+    };
+</script>
---
+++ 8. Component Communication/cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <label>Name</label>
+        <input type="text" />
+    </div>
+    <div>
+        <label>Age</label>
+        <input type="text" />
+    </div>
+</template>
+<script>
+</script>
