9bdef33869a09793373669c551b30ec42ad1138e
das972811 <salasdiego54@gmail.com>
2023-07-06 13:13:03 -0500

    Scoped slots

--- 9. Diving Deeper Into Components/cmp-adv-01-starting-setup/src/App.vue
+++ 9. Diving Deeper Into Components/cmp-adv-01-starting-setup/src/App.vue
@@ -8,6 +8,10 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></UserInfo>
+    <course-goals #default="slotProps">
+        <h2>{{ slotProps.goal }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+    </course-goals>
   </div>
 </template>
 
@@ -15,12 +19,14 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   components: {
     TheHeader,
     BadgeList,
-    UserInfo
+    UserInfo,
+    CourseGoals,
   },
   data() {
     return {
