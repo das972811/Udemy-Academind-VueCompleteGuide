8be678a78b7befeee3608e11c93b8c4d4f68d62e
das972811 <salasdiego54@gmail.com>
2023-07-10 21:37:58 -0500

    Renamed files

---
+++ 10. Course Project - The Learning Resource App/prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,25 @@
+<template>
+    <li>
+        <h2>{{ title }}</h2>
+        <p>{{ description }}</p>
+        <p><a :href="link" target="_blank">{{ link }}</a></p>
+    </li>
+</template>
+<script>
+    export default {
+        props: {
+            title: {
+                type: String,
+                required: true,
+            },
+            description: {
+                type: String,
+                required: true,
+            },
+            link: {
+                type: String,
+                required: true,
+            }
+        },
+    }
+</script>
