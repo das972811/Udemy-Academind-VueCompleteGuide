4a203be3ef6c56356c37823bdca3c59f7338c10c
das972811 <salasdiego54@gmail.com>
2023-07-10 21:24:13 -0500

    Added a main app and a componenet

---
+++ 10. Course Project - The Learning Resource App/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul>
+        <resource-item v-for="resource in storedResources"
+        :key="resource.id"
+        :title="resource.title"
+        :description="resource.description"
+        :link="resource.link"
+        ></resource-item>
+    </ul>
+</template>
+<script>
+    import ResourceItem from './components/ResourceItem.vue'
+
+    export default {
+        components: {
+            ResourceItem,
+        },
+        data() {
+            return {
+                storedResources: [
+                    {
+                        id: 'official-guide',
+                        title: 'Official Guide',
+                        description: 'The official Vue.js documentation',
+                        link: 'https://vue.js.org'
+                    },
+                    {
+                        id: 'google',
+                        title: 'Google',
+                        description: 'Learn to google...',
+                        link: 'https://google.org'
+                    }
+                ],
+            };
+        },
+    };
+</script>
